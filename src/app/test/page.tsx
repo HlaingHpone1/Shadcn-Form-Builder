8813a9a4ead79947f73dfddc3e7b2b1c69833b1a
Hlaing Hpone <hlainghpone1@gmail.com>
2025-12-24 10:30:35 +0630

    Fix: export default and change page

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,6 +1,8 @@
-import { useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import * as z from "zod"
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
 
 import {
   Form,
@@ -16,9 +18,8 @@ import { Button } from "@/components/ui/button";
 import { PasswordInput } from "@/components/ui/password-input";
 
 const formSchema = z.object({
-  field_1: z.string().nonempty("New Label is required")
-
-})
+  field_1: z.string().nonempty("New Label is required"),
+});
 
 const data: { id: number; name: string }[] = [
   { id: 1, name: "Option 1" },
@@ -26,45 +27,44 @@ const data: { id: number; name: string }[] = [
   { id: 3, name: "Option 3" },
 ];
 
-export function MyGeneratedForm() {
+function MyGeneratedForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-        field_1: ""
+      field_1: "",
     },
-  })
+  });
 
   function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values)
+    console.log(values);
   }
-
-
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-
-              <FormField
-                control={form.control}
-                name="field_1"
-                render={({ field }) => (
-                  <FormItem className="gap-3">
-                    <FormLabel>
-                      New Label <span className="text-red-500">*</span>
-                    </FormLabel>
-                    <FormControl>
-                      <PasswordInput
-                        autoComplete="off"
-                        placeholder="New Label..."
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+        <FormField
+          control={form.control}
+          name="field_1"
+          render={({ field }) => (
+            <FormItem className="gap-3">
+              <FormLabel>
+                New Label <span className="text-red-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <PasswordInput
+                  autoComplete="off"
+                  placeholder="New Label..."
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
         <Button type="submit">Submit</Button>
       </form>
     </Form>
-  )
+  );
 }
+
+export default MyGeneratedForm;
